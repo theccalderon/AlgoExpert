0dbdc4ada9a4d981e48cb5ad76a6e2bdc3ae0cd6
Cesar Calderon Muro <cesar.calderon.dev@gmail.com>
2023-12-20 23:42:43 -0500

    wip push

--- k_means/main.py
+++ k_means/main.py
@@ -21,7 +21,7 @@ def features_mean(matrix):
 def get_k_means(user_feature_map, num_features_per_user, k):
     # Don't change the following two lines of code.
     random.seed(42)
-    # Gets the inital userrs, to be used as centroids.
+    # Gets the inital users, to be used as centroids.
     inital_centroid_users = random.sample(sorted(list(user_feature_map.keys())), k)
 
     centroids = [Centroid(user_feature_map[i]) for i in inital_centroid_users]

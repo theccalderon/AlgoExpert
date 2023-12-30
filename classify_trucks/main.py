79bd97abbc64057efdcf20aff1002dcbfb538a86
Cesar Calderon Muro <cesar.calderon.dev@gmail.com>
2023-12-30 18:46:34 -0500

    1st commit

---
+++ classify_trucks/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+def classify_trucks():
+    # Write your code here.
+    pass
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

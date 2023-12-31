fb1745f73a382eeba4d61afe3f9f71697433503d
Cesar Calderon Muro <cesar.calderon.dev@gmail.com>
2023-12-31 01:42:03 -0500

    1st commit

---
+++ tldr/main.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from nltk import sent_tokenize
+from nltk.corpus import stopwords
+
+def tldr(text_to_summarize):
+    # Write your code here
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

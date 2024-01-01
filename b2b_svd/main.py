8d50cba5d4aa9b563244cc8a431f467a2914c5c8
Cesar Calderon Muro <cesar.calderon.dev@gmail.com>
2024-01-01 17:08:06 -0500

    1st commit

---
+++ b2b_svd/main.py
@@ -0,0 +1,12 @@
+from sklearn.decomposition import TruncatedSVD
+
+def b2b_svd(avg_trxn_amount, num_monthly_trxn):
+    # Write your code here
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
